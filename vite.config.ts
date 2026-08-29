06f50ff4b72b2f87162858447f1c537436757470
21Sobral <49079321+v21sobral@users.noreply.github.com>
2026-08-29 00:31:38 -0300

    Change default title from 'Figma Make App' to 'Vida Saúde'

--- vite.config.ts
+++ vite.config.ts
@@ -91,7 +91,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? "Figma Make App"
+  const title = config.title ?? "Vida+Saúde"
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
